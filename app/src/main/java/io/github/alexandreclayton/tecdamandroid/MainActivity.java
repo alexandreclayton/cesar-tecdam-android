c06af62a61401cd571c15e5f0193d7706b37cd08
alexandresette <fu30541996>
2017-09-07 16:33:01 -0300

    Refatorando os fontes para PlayNow, PlayList e Profile.

--- app/src/main/java/io/github/alexandreclayton/tecdamandroid/MainActivity.java
+++ app/src/main/java/io/github/alexandreclayton/tecdamandroid/MainActivity.java
@@ -3,13 +3,21 @@ package io.github.alexandreclayton.tecdamandroid;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.widget.TextView;
+
+import io.github.alexandreclayton.tecdamandroid.fragments.PlayListFragment;
+import io.github.alexandreclayton.tecdamandroid.fragments.PlayNowFragment;
+import io.github.alexandreclayton.tecdamandroid.fragments.ProfileFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+
+    private PlayNowFragment playNowFragment;
+    private PlayListFragment playListFragment;
+    private ProfileFragment profileFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +25,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    //mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_playnow:
+                    changeFragment(playNowFragment);
                     return true;
-                case R.id.navigation_dashboard:
-                    //mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_playlist:
+                    changeFragment(playListFragment);
                     return true;
                 case R.id.navigation_profile:
-                    //mTextMessage.setText(R.string.title_profile);
+                    changeFragment(profileFragment);
                     return true;
             }
             return false;
@@ -37,9 +45,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        if (fragmentManager.findFragmentById(R.id.content) == null) {
+            playNowFragment = new PlayNowFragment();
+            playListFragment = new PlayListFragment();
+            profileFragment = new ProfileFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.content, playNowFragment)
+                    .add(R.id.content, playListFragment)
+                    .add(R.id.content, profileFragment)
+                    .hide(playListFragment)
+                    .hide(profileFragment)
+                    .commit();
+        } else {
+            buildFragments(fragmentManager);
+        }
+
+
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void buildFragments(FragmentManager fragmentManager) {
+        playNowFragment = (PlayNowFragment) fragmentManager.getFragments().get(0);
+        playListFragment = (PlayListFragment) fragmentManager.getFragments().get(1);
+        profileFragment = (ProfileFragment) fragmentManager.getFragments().get(2);
+    }
+
+    private void changeFragment(Fragment frg) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .hide(playNowFragment)
+                .hide(playListFragment)
+                .hide(profileFragment)
+                .show(frg).commit();
     }
 
 }
--- app/src/main/java/io/github/alexandreclayton/tecdamandroid/fragments/PlayListFragment.java
+++ app/src/main/java/io/github/alexandreclayton/tecdamandroid/fragments/PlayListFragment.java
@@ -13,12 +13,12 @@ import io.github.alexandreclayton.tecdamandroid.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link PlaslistFragment.OnFragmentInteractionListener} interface
+ * {@link PlayListFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link PlaslistFragment#newInstance} factory method to
+ * Use the {@link PlayListFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class PlaslistFragment extends Fragment {
+public class PlayListFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class PlaslistFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public PlaslistFragment() {
+    public PlayListFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class PlaslistFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment PlaslistFragment.
+     * @return A new instance of fragment PlayListFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static PlaslistFragment newInstance(String param1, String param2) {
-        PlaslistFragment fragment = new PlaslistFragment();
+    public static PlayListFragment newInstance(String param1, String param2) {
+        PlayListFragment fragment = new PlayListFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class PlaslistFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plaslist, container, false);
+        return inflater.inflate(R.layout.fragment_playlist, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
